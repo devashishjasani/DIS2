3d5bbacaef59dc75c90778b6c7c815d1621e35bb
Devashish Jasani <dev1@app334.newton.rwth-aachen.de>
2014-07-04 17:51:27 +0200

    Initial Commit

---
+++ DIS2MobileApplication/XYZFirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  XYZFirstViewController.h
+//  DIS2MobileApplication
+//
+//  Created by Devashish Jasani on 04/07/14.
+//  Copyright (c) 2014 Devashish Jasani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZFirstViewController : UIViewController
+
+@end
---
+++ DIS2MobileApplication/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DIS2MobileApplication
+//
+//  Created by Devashish Jasani on 04/07/14.
+//  Copyright (c) 2014 Devashish Jasani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYZAppDelegate class]));
+    }
+}
