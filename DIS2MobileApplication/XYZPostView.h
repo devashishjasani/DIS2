828bde7e55d4e07b19420a0b2b450929db4f902e
Giorgio <git@giorgiopretto.com>
2014-07-13 18:20:59 +0200

    hi

--- DIS2MobileApplication/XYZPostView.h
+++ DIS2MobileApplication/XYZPostView.h
@@ -19,7 +19,7 @@
 @property (strong, nonatomic) IBOutlet UILabel *showTimings;
 @property (strong, nonatomic) IBOutlet UILabel *imdbRatings;
 @property (strong, nonatomic) IBOutlet UILabel *nowWatching;
-
+@property UILabel *hi;
 @property (weak, nonatomic) IBOutlet UIView *flickableView;
 
 @property (strong, nonatomic) IBOutlet UISwipeGestureRecognizer *swipeUpGesture;
