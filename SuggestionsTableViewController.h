a650ed3b933b55f684e10f726fdb5215e2fb88dd
Giorgio Pretto <git@giorgiopretto.com>
2014-07-15 19:14:28 +0200

    Fix suggestion tab with view controller

---
+++ SuggestionsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SuggestionsTableViewController.h
+//  DIS2MobileApplication
+//
+//  Created by Giorgio Pretto on 7/15/14.
+//  Copyright (c) 2014 Devashish Jasani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SuggestionsTableViewController : UITableViewController
+
+@end
